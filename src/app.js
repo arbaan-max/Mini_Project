16552953528444c935ded6d9e374902f0cac0c50
Arbaan-RoyalMart <arbaan@nextaverse.in>
2024-01-26 14:47:10 +0530

    made a / endpont for testing server

--- src/app.js
+++ src/app.js
@@ -13,9 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api/v1/user", userRoutes,postRouter);
+app.use("/api/v1/user", userRoutes, postRouter);
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server Is Running");
+});
 
 connectDb()
   .then(() => {
