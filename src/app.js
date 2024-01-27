eb4604b1f460378bf8da88832278315d6b34c882
Arbaan-RoyalMart <arbaan@nextaverse.in>
2024-01-27 11:01:37 +0530

    added the health

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use("/api/v1/user", userRoutes, postRouter);
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.send("Server Is Running");
 });
 
